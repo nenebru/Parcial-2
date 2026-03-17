0e9ed61d2d962dd98aae49899f5768361a992a55
nenebru <bruna.luisa.santos@escola.pr.gov.br>
2026-03-17 08:42:05 -0300

    Update desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,1 +1,8 @@
+# Solicitar o número ao usuário
+num = int(input("Digite um número: "))
 
+# Verificar se o número é par ou ímpar
+if num % 2 == 0:
+    print(f"O número {num} é par.")
+else:
+    print(f"O número {num} é ímpar.")
