ffae3fd062723fd203dcef4e742c163e523683e0
nenebru <bruna.luisa.santos@escola.pr.gov.br>
2026-03-17 08:40:27 -0300

    Update desafio1.py

--- desafio1.py
+++ desafio1.py
@@ -1,1 +1,9 @@
+# Solicitar os números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
 
+# Soma os números
+soma = num1 + num2
+
+# Exibe o resultado
+print(f"A soma de {num1} e {num2} é: {soma}")
